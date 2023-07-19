70176fa098e0b312b98cb2e80181c6c9331d12d2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-19 11:28:52 +0200

    feat: add loader function insnde Menu, define menuLoader in App

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,16 @@
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
   return <h1>Menu</h1>;
+}
+
+export async function loader() {
+  try {
+    const menu = await getMenu();
+    return menu;
+  } catch (e) {
+    console.error(e.message);
+  }
 }
 
 export default Menu;
