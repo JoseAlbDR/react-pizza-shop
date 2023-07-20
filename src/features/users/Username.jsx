a00416b406d11c6ef6bb005f2a7c465b29fd0b1f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-20 15:15:58 +0200

    feat: use tailwind flex home and cartOverview

--- src/features/users/Username.jsx
+++ src/features/users/Username.jsx
@@ -1,7 +1,5 @@
 function Username() {
-  return (
-    <div className="hidden text-sm font-semibold md:inline-block">YuSeP</div>
-  );
+  return <div className="hidden text-sm font-semibold md:block">YuSeP</div>;
 }
 
 export default Username;
