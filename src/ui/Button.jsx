143a2110eead20d0c97d9c89a653c835737f5d8a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-21 00:48:22 +0200

    feat: new component LinkButton

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,7 +2,7 @@ function Button({ children, disabled }) {
   return (
     <button
       disabled={disabled}
-      className="rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+      className="sd:px-6 sd:py-4 rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
     >
       {children}
     </button>
