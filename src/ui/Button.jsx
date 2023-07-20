4434b85517d50fef14f29ad34b06a3a88840d618
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-21 00:52:53 +0200

    feat: reuse Button to order pizzas lnk

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,18 @@
-function Button({ children, disabled }) {
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to }) {
+  const className =
+    'sd:px-6 sd:py-4 rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      disabled={disabled}
-      className="sd:px-6 sd:py-4 rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-    >
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   );
