3b67210e634764c28e410717210ac9bb5d909e0c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-22 01:39:39 +0200

    feat: add handleDelete, handleIncrease and handleDecrease functions to CartItem component

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,8 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import {
+  deletePizza,
+  increasePizzaQuantity,
+  decreasePizzaQuantity,
+} from './cartSlice';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const dispatch = useDispatch();
+
+  function handleDelete() {
+    dispatch(deletePizza(pizzaId));
+  }
+
+  function handleIncrease() {
+    dispatch(increasePizzaQuantity(pizzaId));
+  }
+
+  function handleDecrease() {
+    dispatch(decreasePizzaQuantity(pizzaId));
+  }
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -10,8 +29,17 @@ function CartItem({ item }) {
         {quantity}&times; {name}
       </p>
       <div className="flex items-center justify-between sm:gap-6">
+        <Button type="small" onClick={handleDecrease}>
+          -
+        </Button>
+        <p>{quantity}</p>
+        <Button type="small" onClick={handleIncrease}>
+          +
+        </Button>
         <p>{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <Button onClick={handleDelete} type="small">
+          Delete
+        </Button>
       </div>
     </li>
   );
