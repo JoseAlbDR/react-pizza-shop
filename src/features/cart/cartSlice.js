eeeffedb02c03831fb7d811a01e4c354ce6ed1ef
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-22 01:36:37 +0200

    feat: add cartSlice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  // cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: 'Mediterranean',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addPizza(state, action) {
+      state.cart.push(action.payload);
+    },
+    deletePizza(state, action) {
+      state.cart = state.cart.filter(
+        (pizza) => pizza.pizzaId !== action.payload,
+      );
+    },
+    increasePizzaQuantity(state, action) {
+      const pizza = state.cart.find(
+        (pizza) => pizza.pizzaId === action.payload,
+      );
+      pizza.quantity++;
+      pizza.totalPrice = pizza.quantity * pizza.unitPrice;
+    },
+    decreasePizzaQuantity(state, action) {
+      const pizza = state.cart.find(
+        (pizza) => pizza.pizzaId === action.payload,
+      );
+      if (pizza.quantity === 0) return;
+      pizza.quantity--;
+      pizza.totalPrice = pizza.quantity * pizza.unitPrice;
+    },
+    clearCart(state, action) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addPizza,
+  deletePizza,
+  increasePizzaQuantity,
+  decreasePizzaQuantity,
+  clearCart,
+} = cartSlice.actions;
+export default cartSlice.reducer;
