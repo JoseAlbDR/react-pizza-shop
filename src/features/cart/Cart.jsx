1c79c4aa45d4038630fec33e766be6aadd581f37
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-22 01:39:10 +0200

    feat: add handleClear function to Cart component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from './cartSlice';
 
 const fakeCart = [
   {
@@ -29,8 +30,14 @@ const fakeCart = [
 ];
 
 function Cart() {
-  const cart = fakeCart;
+  const { cart } = useSelector((store) => store.cart);
   const username = useSelector((store) => store.user?.username);
+  const dispatch = useDispatch();
+
+  function handleClear() {
+    dispatch(clearCart());
+  }
+
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
@@ -38,16 +45,18 @@ function Cart() {
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
-        {cart.map((item) => (
-          <CartItem item={item} key={item.pizzaId} />
-        ))}
+        {cart.length === 0
+          ? 'Your cart is empty.'
+          : cart.map((item) => <CartItem item={item} key={item.pizzaId} />)}
       </ul>
 
       <div className="mt-10 space-x-2">
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={handleClear}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
