7fa160101e87c4a59bdc92707b786db7c2f90b4f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-22 11:13:46 +0200

    feat: hadleAddPizza function to handle onClick event in Add to cart button in Menu component

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,23 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addPizza } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  function handleAddPizza() {
+    dispatch(
+      addPizza({
+        pizzaId: id,
+        name,
+        quantity: 1,
+        unitPrice,
+        totalPrice: unitPrice * 1,
+      }),
+    );
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -25,7 +40,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddPizza}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
