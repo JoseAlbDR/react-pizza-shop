4ab6484cbc49b0cd1a6750b16ba0710c6f243c19
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-23 00:03:05 +0200

    refactor: create getCart function in cartSlice

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -3,34 +3,10 @@ import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
 import { useDispatch, useSelector } from 'react-redux';
-import { clearCart } from './cartSlice';
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { clearCart, getCart } from './cartSlice';
 
 function Cart() {
-  const { cart } = useSelector((store) => store.cart);
+  const cart = useSelector(getCart);
   const username = useSelector((store) => store.user?.username);
   const dispatch = useDispatch();
 
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -59,6 +59,8 @@ export const {
 } = cartSlice.actions;
 export default cartSlice.reducer;
 
+export const getCart = (state) => state.cart.cart;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((total, item) => total + item.quantity, 0);
 
