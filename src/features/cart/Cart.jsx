1c98f3765a912ba032296cd0b86eb83e50d210f5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-23 00:13:42 +0200

    feat: use EmptyCart component it there are no pizzas in cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -4,6 +4,7 @@ import CartItem from './CartItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart } from './cartSlice';
 import { getUsername } from '../users/userSlice';
+import EmptyCart from './EmptyCart';
 
 function Cart() {
   const cart = useSelector(getCart);
@@ -14,6 +15,8 @@ function Cart() {
     dispatch(clearCart());
   }
 
+  if (!cart.length) return <EmptyCart />;
+
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
@@ -21,9 +24,9 @@ function Cart() {
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
-        {cart.length === 0
-          ? 'Your cart is empty.'
-          : cart.map((item) => <CartItem item={item} key={item.pizzaId} />)}
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
       </ul>
 
       <div className="mt-10 space-x-2">
