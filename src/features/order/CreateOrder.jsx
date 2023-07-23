b486e090303520c5d5d983ee5e1beade54226a11
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-23 23:03:12 +0200

    feat: add Get position button to dispatch fetchAddress action in CreateOrder

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,8 +2,8 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
-import { getUsername } from '../users/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAddress, getUsername } from '../users/userSlice';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import { useState } from 'react';
 import EmptyCart from '../cart/EmptyCart';
@@ -24,6 +24,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
+  const dispatch = useDispatch();
   const totalCartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
@@ -33,6 +34,8 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get position</button>
 
       {/* <Form methond="POST" action="/order/new"> */}
       <Form method="POST">
